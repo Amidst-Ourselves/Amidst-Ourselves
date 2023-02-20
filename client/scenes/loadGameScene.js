4dcf434b293bfc8fca45a9ab0492fca7b5de658f
Logan Vaughan <logan.v90@gmail.com>
2023-02-20 00:24:13 -0700

    inital set up for creating and joining game rooms

---
+++ client/scenes/loadGameScene.js
@@ -0,0 +1,23 @@
+class loadGameScene extends Phaser.Scene {
+    constructor() {
+        super("loadGameScene")
+    }
+
+    init(roomCodeObj) {
+        this.roomCodeObj = roomCodeObj;
+        this.socket = io(SERVER_ADDRESS);
+    }
+    
+    create() {
+        this.socket.emit('roomJoinCreate', this.roomCodeObj);
+
+        this.socket.on('roomJoinCreateResponse', (roomObj) => {
+            if (roomObj.roomCode === undefined) {
+                this.scene.start("titleScene", {message: 'failed to join room'});
+            } else {
+                this.scene.start("gameScene", roomObj);
+            }
+            this.socket.disconnect();
+        });
+    }
+}
--- server/index.js
+++ server/index.js
@@ -11,51 +11,105 @@ const io = new Server(httpServer, {
     }
 });
 
-
-room1 = {
-    name: "room1",
-    speed: 2,
-    players: {}
-};
-
-
-io.on('connection', (socket) => {
-    room1.players[socket.id] = {x: 400, y: 400};
-    console.log(room1);
-
-
-    io.to(room1.name).emit('join', {
-        id: socket.id,
-        x: 400,
-        y: 400
-    });
-
-    socket.join(room1.name);
-
-    socket.on('requestRoom', () => {
-        socket.emit('room', room1);
-    })
-
-    socket.on('disconnect', () => {
-        delete room1.players[socket.id];
-        io.to(room1.name).emit('leave', {
-            id: socket.id
-        });
-    })
-
-    socket.on('move', (data) => {
-        id = socket.id;
-        socket.broadcast.to(room1.name).emit('move', {
-            id: socket.id,
-            x: data.x,
-            y: data.y
-        });
-        room1.players[socket.id].x = data.x;
-        room1.players[socket.id].y = data.y;
-    });
-});
-
-
 httpServer.listen(3000, () => {
     console.log('listening on localhost:3000');
 });
+
+
+
+
+const ROOM_CODE_LENGTH = 4;
+const ROOM_CODE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+const ROOM_CODE_CHARACTERS_LENGTH = ROOM_CODE_CHARACTERS.length;
+
+let rooms = {};
+
+
+
+
+io.on('connection', (socket) => {
+    socket.on('roomJoinCreate', (roomCodeObj) => {
+        let roomObj;
+        if (roomCodeObj.roomCode === undefined) {
+            roomObj = createRoom();
+        } else {
+            roomObj = getRoom(roomCodeObj.roomCode);
+        }
+        socket.emit('roomJoinCreateResponse', roomObj);
+    });
+
+    socket.on('roomJoin', (roomCodeObj) => {
+        if (roomCodeObj.roomCode === undefined || !roomCodeObj.roomCode in rooms) {
+            socket.emit('roomJoinResponse', {});
+            return;
+        }
+
+        let room = rooms[roomCodeObj.roomCode];
+
+        if (room.players === undefined) {
+            players = {};
+            players[socket.id] = {x: 400, y: 400};
+            room["host"] = socket.id;
+            room["players"] = players;
+        } else {
+            room.players[socket.id] = {x: 400, y: 400};
+        }
+
+        io.to(roomCodeObj.roomCode).emit('join', {id: socket.id, x: 400, y: 400});
+        socket.join(roomCodeObj.roomCode);
+        socket.emit('roomJoinResponse', rooms[roomCodeObj.roomCode]);
+        socket.roomCode = roomCodeObj.roomCode;
+
+        console.log(rooms);
+    });
+
+    socket.on('disconnect', () => {
+        if (socket.roomCode === undefined) {
+            // do nothing
+        } else {
+            delete rooms[socket.roomCode].players[socket.id];
+            io.to(socket.roomCode).emit('leave', {id: socket.id});
+        }
+    });
+
+    socket.on('move', (playerObj) => {
+        socket.broadcast.to(socket.roomCode).emit('move', {
+            id: socket.id,
+            x: playerObj.x,
+            y: playerObj.y
+        });
+        rooms[socket.roomCode].players[socket.id].x = playerObj.x;
+        rooms[socket.roomCode].players[socket.id].y = playerObj.y;
+    });
+});
+
+
+function createRoom() {
+    let roomCode = createRoomCode();
+    let newRoom = {
+        roomCode: roomCode,
+        speed: 2
+    }
+    rooms[roomCode] = newRoom;
+    return rooms[roomCode];
+}
+
+
+function getRoom(roomCode) {
+    if (roomCode in rooms) {
+        return rooms[roomCode];
+    }
+    return {};
+}
+
+
+function createRoomCode() {
+    let roomCode;
+    do {
+        roomCode = '';
+        for (let i=0; i<ROOM_CODE_LENGTH; i++) {
+            roomCode += ROOM_CODE_CHARACTERS[Math.floor(Math.random() * ROOM_CODE_CHARACTERS_LENGTH)];
+        }
+    } while (roomCode in rooms);
+    return roomCode;
+}
