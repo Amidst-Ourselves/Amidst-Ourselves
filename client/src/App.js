130ae18830198c4f2ff8a6b981aae44301d15f94
Chanpreet Singh <55308509+Chanpreet-Singh-UofA@users.noreply.github.com>
2023-02-21 11:39:08 -0700

    Username/pass base
    Username/pass base

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Phaser from 'phaser';
-import titleScene from './client/scenes/titleScene';
-import loadGameScene from './client/scenes/loadGameScene';
-import gameScene from './client/scenes/gameScene';
+import './App.css';
+import titleScene from './amidstOurselvesGame/scenes/titleScene';
+import loadGameScene from './amidstOurselvesGame/scenes/loadGameScene';
+import gameScene from './amidstOurselvesGame/scenes/gameScene';
+
+import { Login } from './login';
+import { Register } from './register';
+
 const SPRITE_WIDTH = 84;
 const SPRITE_HEIGHT = 128;
 const PLAYER_WIDTH = 34;
 const PLAYER_HEIGHT = 46;
 const SERVER_ADDRESS = 'http://localhost:3000';
 
+function MyGame() {
+  const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 800,
+    scene: [titleScene, loadGameScene, gameScene],
+    parent: 'game-container',
+  };
 
-class MyGame extends Phaser.Game {
-  constructor() {
-    const config = {
-      type: Phaser.AUTO,
-      width: 800,
-      height: 800,
-      scene: [titleScene, loadGameScene, gameScene],
-      parent: 'game-container',
+  const game = new Phaser.Game(config);
+
+  useEffect(() => {
+    return () => {
+      game.destroy(true);
     };
+  }, [game]);
 
-    super(config);
-  }
+  return null;
 }
 
-export default class App extends Component {
-  game = null;
+export default function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  componentDidMount() {
-    this.game = new MyGame();
-  }
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
 
-  componentWillUnmount() {
-    this.game.destroy(true);
-  }
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
 
-  render() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Amidst Ourselves</h1>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} id="game-container"></div>
-      </div>
-    );
-  }
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Amidst Ourselves</h1>
+      {loggedIn ? (
+        <div
+          style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+          id="game-container"
+        >
+          <MyGame />
+        </div>
+      ) : (
+        <div className="App">
+          {currentForm === 'login' ? (
+            <Login onFormSwitch={toggleForm} onLogin={handleLogin} />
+          ) : (
+            <Register onFormSwitch={toggleForm} onLogin={handleLogin} />
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
-
-
-
