18c88f41c6517d95e5b937b5b60f3ac4660baa44
Logan Vaughan <logan.v90@gmail.com>
2023-04-04 23:02:14 -0600

    adding comment back

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./API/user"));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
 const httpServer = createServer(app);
 
