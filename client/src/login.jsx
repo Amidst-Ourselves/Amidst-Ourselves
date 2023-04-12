a648542c7a9352e28cdf95b4fef4921b287408dd
Chanpreet Singh <55308509+Chanpreet-Singh-UofA@users.noreply.github.com>
2023-04-11 19:14:33 -0600

    Made fileds required.
    Made fileds required.

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -53,10 +53,10 @@ export const Login = (props) => {
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" required/>
                 <br></br> <p></p>
                 <label htmlFor="password">Password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" required/>
                 <br></br> <p></p>
                 <button type="submit">Log In</button>
                 {errorMessage && (
